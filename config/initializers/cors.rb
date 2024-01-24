63ce9315abb5d29f33ea7f70501ebda4774eb950
forrest <liftplusultra@gmail.com>
2024-01-23 17:45:22 -0700

    Remove ActiveRecord usage from cors.rb.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins *Account.active.map(&:domain).map(&:strip)
+    #TODO update to use a cached list of allowed origins.
+    origins "*"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
